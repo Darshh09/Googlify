47a5656a834b00884e5a49f54c319633fd879c94
Darshh09 <Darshitshukla1777@gmail.com>
2021-10-27 01:52:15 +0530

    First Commit Added Dark Theme

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import ReactDom from 'react-dom';
+import './global.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+
+ReactDom.render(
+<Router>
+<App/>
+</Router>
+, document.getElementById('root'));
